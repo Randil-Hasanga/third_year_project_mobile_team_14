e22de3e885ee9506b0448a86b7954b1af63f4046
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-28 22:13:43 +0530

    Screen package added to buil screen separately

---
+++ job_management_system_mobileapp/lib/Screens/HomeScreen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(250.0),
+          child: AppBar(
+            backgroundColor: const Color.fromARGB(255, 241, 157, 47),
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/DME_image01.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: Container(
+          color: const Color.fromARGB(255, 241, 157, 47),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 350,
+                  height: 70,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "ආයුබෝවන් සිංහලෙන් බලන්න",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: 'AbhayaLibre',
+                          ),
+                        ),
+                        Icon(Icons.arrow_forward),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                SizedBox(
+                  width: 350,
+                  height: 70,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "English",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: 'AbhayaLibre',
+                          ),
+                        ),
+                        Icon(Icons.arrow_forward),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                SizedBox(
+                  width: 350,
+                  height: 70,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "தமிழில் ஆயுபோவன் பார்க்கவும்",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'AbhayaLibre',
+                          ),
+                        ),
+                        Icon(Icons.arrow_forward),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- job_management_system_mobileapp/lib/main.dart
+++ job_management_system_mobileapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:job_management_system_mobileapp/Screens/HomeScreen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,116 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: Size.fromHeight(250.0),
-            child: AppBar(
-              backgroundColor: const Color.fromARGB(255, 241, 157, 47),
-              flexibleSpace: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/DME_image01.png"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-          // Body of the mobile App
-          body: Container(
-            color: const Color.fromARGB(255, 241, 157, 47),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 350,
-                    height: 70,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.orange, // Button background color
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15), // Set button border radius
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "ආයුබෝවන් සිංහලෙන් බලන්න",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: 'AbhayaLibre',
-                            ),
-                          ),
-                          Icon(Icons.arrow_forward),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  SizedBox(
-                    width: 350,
-                    height: 70,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.orange,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "English",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: 'AbhayaLibre',
-                            ),
-                          ),
-                          Icon(Icons.arrow_forward),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  SizedBox(
-                    width: 350,
-                    height: 70,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.orange,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "தமிழில் ஆயுபோவன் பார்க்கவும்",
-                            style: TextStyle(
-                              fontSize: 17,
-                              fontFamily: 'AbhayaLibre',
-                            ),
-                          ),
-                          Icon(Icons.arrow_forward),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
