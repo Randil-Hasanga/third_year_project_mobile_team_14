a2189f5ca702f95e982d9150a07dc8408c4b1037
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-28 14:51:42 +0530

    create elevatedButton with multyple language

--- job_management_system_mobileapp/lib/main.dart
+++ job_management_system_mobileapp/lib/main.dart
@@ -11,22 +11,84 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar:AppBar(
-         
+      debugShowCheckedModeBanner: false,
+      home:  Scaffold( 
+
+
+//AppBar with Department of Manpower and Employement Banner
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(200.0), // Corrected capitalization here
+        child: AppBar(
+          backgroundColor: const Color.fromARGB(255, 241, 157, 47),
           flexibleSpace: Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage("assets/DME_image01.png"),
-                fit: BoxFit.cover  ),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          
-         
         ),
+      ),
 
-        body:Center(child: Text("JOB CENTER "),
+//body of the App
+body:Container(
+  color: Color.fromARGB(255, 247, 153, 12),
+  child: Center(
+    
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 250,
+          height: 70,
         ),
+  //button to navigate to sinhala      
+        ElevatedButton(
+          onPressed: (){
+  
+          },
+           child: Text("ආයුබොවන් සිංහලෙන් බලන්න",
+           style: TextStyle(
+            fontSize: 20,
+            fontFamily: 'AbhayaLibre',
+           ),
+           ),
+        ),
+  
+  
+        SizedBox(height: 10),
+        ElevatedButton(onPressed: (){},
+         child:Text(
+          "English",
+          style: TextStyle(
+            fontSize: 20,
+          
+          ),
+         ),
+         ),
+  
+  
+  
+          SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle button press
+                  },
+                  child: Text(
+                    "தமிழில் ஆயுபோவன் பார்க்கவும்",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'AbhayaLibre',
+                    ),
+                  ),
+                ),
+      ],
+    ),
+  ),
+),
+
+
       ),
     );
   }
