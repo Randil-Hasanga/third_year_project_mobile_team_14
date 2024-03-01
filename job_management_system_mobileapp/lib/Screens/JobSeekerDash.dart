96bec4f504a88a5f2c2436a5df528c3bb481e74c
BANDARAKDT <deshanibandarawas@gmail.com>
2024-03-01 08:24:12 +0530

    screens of mobile app

---
+++ job_management_system_mobileapp/lib/Screens/JobSeekerDash.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+
+
+// class JobSeekerDash extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+//     throw UnimplementedError();
+//   }
+//   @override
+// }
