b167f5293cf69f521287666c4663c9789a4153e7
Dinuka2K <dinuka_2020693@fot.ruh.ac.lk>
2024-03-19 05:09:18 +0530

    create splash screen

--- job_management_system_mobileapp/lib/main.dart
+++ job_management_system_mobileapp/lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:job_management_system_mobileapp/Screens/LogInPage.dart';
+import 'package:job_management_system_mobileapp/Screens/splash_screen.dart';
 
-void main() => runApp(
-  const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: LogInPage(),
-  )
-);
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    ));
