872672ac5716e4738c1af996d45380c81601ddf4
Dinuka2K <dinuka_2020693@fot.ruh.ac.lk>
2024-03-19 11:45:14 +0530

    create onboard

---
+++ job_management_system_mobileapp/lib/Screens/splash_screen.dart
@@ -0,0 +1,29 @@
+import "package:animated_splash_screen/animated_splash_screen.dart";
+import "package:flutter/material.dart";
+import "package:job_management_system_mobileapp/Screens/LogInPage.dart";
+import "package:job_management_system_mobileapp/Screens/onboardScreen.dart";
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Column(
+        children: [
+          Image.asset(
+            "assets/logo.png",
+            height: MediaQuery.of(context).size.width / 2.5,
+            width: MediaQuery.of(context).size.width / 2.5,
+          ),
+          //SizedBox(height: 20),
+          const Text("Job Management System",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        ],
+      ),
+      nextScreen: OnboardScreen(),
+      splashIconSize: 400,
+      backgroundColor: Colors.orange.shade900,
+    );
+  }
+}
