05b52e717ccd436c9f8fc46cf15e1a3832f273b3
Dinuka2K <dinuka_2020693@fot.ruh.ac.lk>
2024-04-20 20:07:40 +0530

    finish interview scheduler ui
    add participants
    add type of interview
    add date and time picker
    submit button

--- job_management_system_mobileapp/lib/Screens/JobProviderScreens/interview_scheduler.dart
+++ job_management_system_mobileapp/lib/Screens/JobProviderScreens/interview_scheduler.dart
@@ -1,17 +1,61 @@
+import "package:board_datetime_picker/board_datetime_picker.dart";
 import "package:flutter/material.dart";
 import "package:job_management_system_mobileapp/localization/demo_localization.dart";
+import "package:job_management_system_mobileapp/services/firebase_services.dart";
+import "package:quickalert/models/quickalert_type.dart";
+import "package:quickalert/widgets/quickalert_dialog.dart";
 
 class InterviewScheduler extends StatefulWidget {
-  const InterviewScheduler({super.key});
+  InterviewScheduler({super.key});
+
+  final FirebaseService _firebaseService = FirebaseService();
 
   @override
   State<InterviewScheduler> createState() => _InterviewSchedulerState();
 }
 
 class _InterviewSchedulerState extends State<InterviewScheduler> {
+  String groupValue = "Online";
+
+  DateTime _selectedDateTime = DateTime.now();
+
+  void _chooseDateTime() async {
+    final result = await showBoardDateTimePicker(
+      context: context,
+      pickerType: DateTimePickerType.datetime,
+      initialDate: _selectedDateTime,
+      options: BoardDateTimeOptions(
+        languages: const BoardPickerLanguages(
+          today: 'Today',
+          tomorrow: 'Tomorrow',
+          now: 'Now',
+        ),
+        startDayOfWeek: DateTime.monday,
+        pickerFormat: PickerFormat.ymd,
+        activeColor: Colors.blue.shade200,
+        backgroundDecoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+      ),
+    );
+    if (result != null) {
+      setState(() {
+        _selectedDateTime = result;
+      });
+    }
+  }
+
+  void showAlert() {
+    QuickAlert.show(
+      context: context,
+      type: QuickAlertType.success,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -56,6 +100,115 @@ class _InterviewSchedulerState extends State<InterviewScheduler> {
                 ),
                 const SizedBox(height: 20.0),
                 //add participants
+                DropdownButtonFormField(
+                  decoration: InputDecoration(
+                    labelText: "Add Participants",
+                    hintText: "Select Participants",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  items: const [
+                    DropdownMenuItem(
+                      value: "Participant 1",
+                      child: Text("Participant 1"),
+                    ),
+                    DropdownMenuItem(
+                      value: "Participant 2",
+                      child: Text("Participant 2"),
+                    ),
+                    DropdownMenuItem(
+                      value: "Participant 3",
+                      child: Text("Participant 3"),
+                    ),
+                  ],
+                  onChanged: (value) {},
+                ),
+                const SizedBox(height: 20.0),
+                // select iterview type
+                Row(
+                  children: [
+                    Radio(
+                      value: "Online",
+                      groupValue: groupValue,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            groupValue = value!;
+                          },
+                        );
+                      },
+                    ),
+                    const Text(
+                      "Online",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(width: 30.0),
+                    Radio(
+                      value: "Physical",
+                      groupValue: groupValue,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            groupValue = value!;
+                          },
+                        );
+                      },
+                    ),
+                    const Text(
+                      "Physical",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                //date and time pickers
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: _chooseDateTime,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.lightBlueAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        child: const Text(
+                          "Select Date and Time",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20.0),
+                    /*Text(
+                        _selectedDateTime.toString(),
+                        ),*/
+                  ],
+                ),
+                const SizedBox(height: 80.0),
+                ElevatedButton(
+                  onPressed: () {
+                    showAlert();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
