a5fcd374b5d9e71c9b4edab987e224ec0402ab33
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2024-04-28 01:23:27 +0530

    JobMatching

---
+++ job_management_system_mobileapp/lib/widgets/richTextWidgets.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class RichTextWidget {
+  RichTextWidget();
+
+  Widget simpleText(
+      String text, double? fontSize, Color color, FontWeight? fontweight) {
+    return RichText(
+      softWrap: true,
+      text: TextSpan(
+        text: text,
+        style: TextStyle(
+          fontSize: fontSize,
+          fontFamily: GoogleFonts.poppins().fontFamily,
+          color: color,
+          fontWeight: fontweight,
+        ),
+      ),
+    );
+  }
+
+  Widget simpleTextWithIconLeft(IconData icon, String text, double? fontSize,
+      Color color, FontWeight? fontweight) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(icon),
+        SizedBox(
+          width: 10,
+        ),
+        RichText(
+          softWrap: true,
+          text: TextSpan(
+            text: text,
+            style: TextStyle(
+              fontSize: fontSize,
+              fontFamily: GoogleFonts.poppins().fontFamily,
+              color: color,
+              fontWeight: fontweight,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget simpleTextWithIconRight(IconData icon, String text, double? fontSize,
+      Color color, FontWeight? fontweight) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        RichText(
+          softWrap: true,
+          text: TextSpan(
+            text: text,
+            style: TextStyle(
+              fontSize: fontSize,
+              fontFamily: GoogleFonts.poppins().fontFamily,
+              color: color,
+              fontWeight: fontweight,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 6,
+        ),
+        Icon(icon),
+      ],
+    );
+  }
+
+  Widget KeyValuePairrichText(
+      String keyText, String valueText, double fontSize) {
+    return Column(
+      children: [
+        Table(
+          columnWidths: const {
+            0: FixedColumnWidth(200),
+          },
+          children: [
+            TableRow(
+              children: [
+                TableCell(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: RichText(
+                      text: TextSpan(
+                        text: keyText,
+                        style: TextStyle(
+                          fontSize: fontSize,
+                          fontFamily: GoogleFonts.poppins().fontFamily,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                TableCell(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: RichText(
+                      text: TextSpan(
+                        text: valueText,
+                        style: TextStyle(
+                          fontSize: fontSize,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: GoogleFonts.poppins().fontFamily,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Divider(),
+      ],
+    );
+  }
+}
