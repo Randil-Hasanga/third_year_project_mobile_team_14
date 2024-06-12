0e682197e8b6f3da255073efee0594601ff832e8
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2024-06-12 11:34:30 +0530

    UI_Mods

--- job_management_system_mobileapp/lib/colors/colors.dart
+++ job_management_system_mobileapp/lib/colors/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
 const appBarColor = Color.fromARGB(255, 255, 106, 47);
-const ScaffoldColor = Color.fromARGB(235, 255, 239, 217);
+const ScaffoldColor = Color.fromARGB(235, 255, 255, 255);
 const cardBackgroundColorLayer3 = Color.fromARGB(255, 255, 255, 255);
 const cardBackgroundColorLayer4 = Color.fromARGB(55, 156, 156, 156);
 const buttonDefaultColorOrange = Color.fromARGB(255, 255, 181, 120);
