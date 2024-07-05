bb88ef0fd89c0e8de91c1612bad7ca7d384f7798
BANDARAKDT <deshanibandarawas@gmail.com>
2024-07-05 21:46:53 +0530

    CVUpload screen

---
+++ job_management_system_mobileapp/lib/Screens/JobSeekerScreens/CVUpload.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CVUpload extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Upload CV'),
+      ),
+      body: Center(
+        child: Text('CV Upload Screen'),
+      ),
+    );
+  }
+}
